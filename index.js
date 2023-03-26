94f2eb4f3ff1800d50ff14cfd1b09ad4972c5d00
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-03-25 22:05:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code here
+/*
 const dodger = document.getElementById("dodger");
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
@@ -27,3 +28,33 @@ function moveDodgerLeft() {
       moveDodgerRight();
     }
   });
+  */
+ 
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers);
+  if (left < 360) {
+    dodger.style.left = `${left+1}px`
+  }   
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  else if (e.key === 'ArrowRight') {
+    moveDodgerRight()
+  }
+});
+
